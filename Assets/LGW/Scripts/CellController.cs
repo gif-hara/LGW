15330ef79c2bcbf099df7cca44e6563a07c2e67f
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2018-06-14 15:04:54 +0100

    :new: プロジェクト追加

---
+++ Assets/LGW/Scripts/CellController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace LGW
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class CellController : MonoBehaviour
+    {
+        private Transform cachedTransform;
+
+        private void Awake()
+        {
+            this.cachedTransform = this.transform;
+            this.cachedTransform.localScale = Vector3.one * UserSettings.Instance.CellSize;
+        }
+    }
+}
---
+++ Assets/LGW/Scripts/UserSettings.cs
@@ -0,0 +1,53 @@
+﻿using HK.Framework.Systems;
+using System;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace LGW
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Serializable]
+    public sealed class UserSettings
+    {
+        private const string KeyName = "LGW.UserSettings";
+
+        public static UserSettings Instance
+        {
+            get
+            {
+                var instance = SaveData.GetClass<UserSettings>(KeyName, null);
+                if(instance == null)
+                {
+                    instance = new UserSettings();
+                    SaveData.SetClass(KeyName, instance);
+                    SaveData.Save();
+                }
+
+                return instance;
+            }
+        }
+
+        [SerializeField]
+        private int cellSize = 20;
+        public int CellSize
+        {
+            get
+            {
+                return this.cellSize;
+            }
+            set
+            {
+                this.cellSize = value;
+                Save();
+            }
+        }
+
+        private static void Save()
+        {
+            SaveData.SetClass(KeyName, Instance);
+            SaveData.Save();
+        }
+    }
+}
